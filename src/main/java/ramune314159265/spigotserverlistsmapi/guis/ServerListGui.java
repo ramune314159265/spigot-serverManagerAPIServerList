e13a46b192d0ed9d5158f41f2d17a2e7babf9bf3
ramune314159265 <ramune314159265@gmail.com>
2024-04-14 10:08:44 +0900

    閉じる追加

--- src/main/java/ramune314159265/spigotserverlistsmapi/guis/ServerListGui.java
+++ src/main/java/ramune314159265/spigotserverlistsmapi/guis/ServerListGui.java
@@ -17,32 +17,42 @@ import java.util.Objects;
 
 public class ServerListGui implements WebSocket.Listener {
 	private final Inventory inventory;
+	private final Byte size;
 
 	public ServerListGui() {
-		this.inventory = Bukkit.createInventory(null, 9, "&aサーバーを選択");
+		this.size = 9;
+		this.inventory = Bukkit.createInventory(null, this.size, "&aサーバーを選択");
 
 		this.init();
 	}
 
 	public void init() {
 		HashMap<String, ServerData> servers = ServerList.get();
-		servers.forEach((k,v)->Bukkit.getLogger().info(k + v.name));
+		servers.forEach((k, v) -> Bukkit.getLogger().info(k + v.name));
 		ServerData proxy = servers.get(ServerListSMapi.proxyId);
 		Bukkit.getLogger().info(ServerList.servers.toString());
 		for (String id : proxy.childIds) {
 			ServerData server = ServerList.servers.get(id);
 			Material material = Objects.requireNonNullElse(Material.matchMaterial(server.minecraftItemIcon), Material.STONE);
-			ItemStack item = new ItemStack(material, 1);
+			ItemStack item = new ItemStack(material, Math.max(1, server.players.length));
 			ItemMeta meta = item.getItemMeta();
 
-			meta.setDisplayName("&a" + Objects.requireNonNullElse(server.name,"不明"));
+			meta.setDisplayName("&a" + Objects.requireNonNullElse(server.name, "不明"));
 			meta.setLore(Arrays.asList("ID: " + server.id, server.description));
 			item.setItemMeta(meta);
 
 			this.inventory.addItem(item);
 		}
+		ItemStack closeItem = new ItemStack(Material.BARRIER, 1);
+		ItemMeta meta = closeItem.getItemMeta();
+
+		meta.setDisplayName("閉じる");
+		closeItem.setItemMeta(meta);
+
+		this.inventory.setItem(this.size - 1, closeItem);
 	}
-	public void open(Player player){
+
+	public void open(Player player) {
 		player.openInventory(this.inventory);
 	}
 }
